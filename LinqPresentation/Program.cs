19596610e23510e503a8e23747524d42f52d6100
Tomasz <miktomi15@gmail.com>
2016-04-06 22:33:39 +0200

    Dalsza część funkcjonalności Dodano: - klasę pomocniczą Extensions, - (częściowo) przykłady użycia LINQ.

--- LinqPresentation/Program.cs
+++ LinqPresentation/Program.cs
@@ -36,7 +36,7 @@ namespace LinqPresentation
 			//}
 
 			watch.Stop();
-			System.Console.WriteLine($" [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			System.Console.WriteLine($"Where [NoLINQ]: {watch.ElapsedMilliseconds}ms");
 			#endregion
 
 			#region | LINQ |
@@ -45,7 +45,7 @@ namespace LinqPresentation
 			//var whereL = school.Students.Where(s => (s.ClassId >= 10) && (s.ClassId <= 30));
 
 			watch.Stop();
-			System.Console.WriteLine($" [LINQ]: {watch.ElapsedMilliseconds}ms");
+			System.Console.WriteLine($"Where [LINQ]: {watch.ElapsedMilliseconds}ms");
 			#endregion
 
 			//System.Console.WriteLine($"Where: {whereNL.Count} vs. {whereL.Count()}");
@@ -72,7 +72,7 @@ namespace LinqPresentation
 
 			#region | Case 01 |
 
-			//sumL = school.Teachers.Sum(t => t.EmploymentDate?.Year ?? 0);
+			// sumL = school.Teachers.Sum(t => t.EmploymentDate?.Year ?? 0);
 
 			#endregion
 			#region | Case 02 |
@@ -88,6 +88,213 @@ namespace LinqPresentation
 			#endregion
 
 			//System.Console.WriteLine($"Sum: {sumNL} vs. {sumL}");
+			#endregion
+
+			#region | Max |
+			int maxNL = -1, maxL = -1;
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+			foreach (var student in school.Students)
+			{
+				if (student.ClassId.HasValue && (student.ClassId.Value > maxNL))
+				{
+					maxNL = (int)student.ClassId.Value;
+				}
+			}
+
+			watch.Stop();
+			System.Console.WriteLine($"Max [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+			maxL = (int)school.Students.
+				Where(s => s.ClassId.HasValue).
+				Max(x => x.ClassId.Value);
+
+			watch.Stop();
+			System.Console.WriteLine($"Max [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			System.Console.WriteLine($"Max: {maxNL} vs. {maxL}");
+			#endregion
+
+			#region | Select |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Select [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+			
+
+			watch.Stop();
+			System.Console.WriteLine($"Select [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"Select: {0} vs. {0}");
+			#endregion  //empty
+
+			#region | OrderBy |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"OrderBy [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"OrderBy [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"OrderBy: {0} vs. {0}");
+			#endregion
+
+			#region | GroupBy |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"GroupBy [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+			//var groupByL = school.Students.
+			//	Where(s => s.ClassId.HasValue).
+			//	GroupBy(s => s.ClassId.Value.IsEven());
+
+			// foreach (var group in groupByL)
+			// {
+			// 	System.Console.WriteLine("IsEven = {group.Key}:");
+			// 
+			// 	foreach (var value in group)
+			// 	{
+			// 		System.Console.Write($"{value} ");
+			// 	}
+			// 
+			// 	System.Console.WriteLine(string.Empty);
+			// }
+
+			watch.Stop();
+			System.Console.WriteLine($"GroupBy [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"GroupBy: {0} vs. {0}");
+			#endregion
+
+			#region | Skip |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Skip [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Skip [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"Skip: {0} vs. {0}");
+			#endregion
+
+			#region | Take |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Take [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Take [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"Take: {0} vs. {0}");
+			#endregion
+
+			#region | Any |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Any [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Any [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"Any: {0} vs. {0}");
+			#endregion
+
+			#region | Reverce |
+
+			#region | NoLINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+			
+
+			watch.Stop();
+			System.Console.WriteLine($"Reverce [NoLINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			#region | LINQ |
+			watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+			watch.Stop();
+			System.Console.WriteLine($"Reverce [LINQ]: {watch.ElapsedMilliseconds}ms");
+			#endregion
+
+			// System.Console.WriteLine($"Reverce: {0} vs. {0}");
 			#endregion
 
 			#region |  |
---
+++ LinqPresentation/Utilities/Extensions.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqPresentation.Utilities
+{
+	public static class Extensions
+	{
+		#region | Done |
+
+		#region IsEven
+
+		public static bool IsEven(this sbyte value) => (value % 2 == 0);
+		public static bool IsEven(this byte value) => (value % 2 == 0);
+		public static bool IsEven(this short value) => (value % 2 == 0);
+		public static bool IsEven(this ushort value) => (value % 2 == 0);
+		public static bool IsEven(this int value) => (value % 2 == 0);
+		public static bool IsEven(this uint value) => (value % 2 == 0);
+		public static bool IsEven(this long value) => (value % 2 == 0);
+		public static bool IsEven(this ulong value) => (value % 2 == 0);
+
+		#endregion
+
+		#endregion
+	}
+}
